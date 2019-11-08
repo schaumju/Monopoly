b6b5720b532a44c8928d021c9de789339b9f729a
Andrew Lee <al042@bucknell.edu>
2019-11-08 11:47:28 -0500

    Made the class for property

---
+++ csci205FinalProject/src/Objects/Utilities.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 11/8/19
+ * Time: 11:17 AM
+ *
+ * Project: csci205FinalProject
+ * Package: Objects
+ * Class: Utilities
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package Objects;
+
+public class Utilities {
+}
