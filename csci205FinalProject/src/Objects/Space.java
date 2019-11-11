d5e1eb13e25525a6eaa1b6dbe48381379275b25d
Ashlyn Ramos <arr016@bucknell.edu>
2019-11-11 11:16:12 -0500

    added Property and Space Classes

---
+++ csci205FinalProject/src/Objects/Space.java
@@ -0,0 +1,34 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 11/11/2019
+ * Time: 11:05 AM
+ *
+ * Project: csci205FinalProject
+ * Package: Objects
+ * Class: Space
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package Objects;
+
+/**
+ * An abstract class that represents a space on the board
+ * @author Ashlyn Ramos
+ */
+public abstract class Space {
+
+    int position;
+    String name;
+
+    public Space(int position, String name) {
+        this.position = position;
+        this.name = name;
+    }
+}
