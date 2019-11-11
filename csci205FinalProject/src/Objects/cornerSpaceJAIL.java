73186c3945e6e92f2218e40d944706bfb3fd390a
Andrew Lee <al042@bucknell.edu>
2019-11-11 14:33:46 -0500

    CornerSpace Parking and Jail finished

---
+++ csci205FinalProject/src/Objects/cornerSpaceJAIL.java
@@ -0,0 +1,29 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 11/11/19
+ * Time: 11:53 AM
+ *
+ * Project: csci205FinalProject
+ * Package: Objects
+ * Class: cornerSpaceJAIL
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package Objects;
+
+public class cornerSpaceJAIL extends Space
+{
+    //Prisoner
+    public cornerSpaceJAIL()
+    {
+        super(9, "JAIL");
+
+    }
+}
