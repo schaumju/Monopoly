d802890210c8ac9b1bf39cb40f6fe7a2c4d2870b
Kerri Palphreyman <kap031@bucknell.edu>
2019-11-14 18:27:24 -0500

    board with names (correct format)

--- csci205FinalProject/src/Game/Spaces/CommunityChest.java
+++ csci205FinalProject/src/Game/Spaces/CommunityChest.java
@@ -5,6 +5,6 @@ import Game.Spaces.Space;
 public class CommunityChest extends Space {
 
     public CommunityChest(int position) {
-        super(position, "Community Chess");
+        super(position, "Community Chest");
     }
 }
