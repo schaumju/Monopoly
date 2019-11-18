1ea6f1eeb01e4d89f03b0d95755e3ab75f678ebf
Kerri Palphreyman <kap031@bucknell.edu>
2019-11-18 11:45:25 -0500

    refactor the view class created CharacterView, DiceView, and MainView

---
+++ csci205FinalProject/src/View/CharacterView.java
@@ -0,0 +1,27 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: Kerri Palphreyman
+ * Section: 11am
+ * Date: 11/18/19
+ * Time: 11:42 AM
+ *
+ * Project: csci205FinalProject
+ * Package: View
+ * Class: CharacterView
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package View;
+
+public class CharacterView {
+
+    protected static void addCharacters() {
+        System.out.println("adding characters");
+    }
+
+}
