00db6b343d22828401b27e4f0c086e7a6c9a98a3
Kerri Palphreyman <kap031@bucknell.edu>
2019-11-20 11:13:04 -0500

    refactored controller package

---
+++ csci205FinalProject/src/Controller/DiceController.java
@@ -0,0 +1,23 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: Kerri Palphreyman
+ * Section: 11am
+ * Date: 11/18/19
+ * Time: 8:30 PM
+ *
+ * Project: csci205FinalProject
+ * Package: Controller
+ * Class: DiceController
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package Controller;
+
+public class DiceController {
+
+}
---
+++ csci205FinalProject/src/Controller/MainController.java
@@ -0,0 +1,23 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: Kerri Palphreyman
+ * Section: 11am
+ * Date: 11/18/19
+ * Time: 8:28 PM
+ *
+ * Project: csci205FinalProject
+ * Package: Controller
+ * Class: MainController
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package Controller;
+
+public class MainController {
+
+}
