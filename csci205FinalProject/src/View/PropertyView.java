48b342f2eb241c95de87ecc0e3f6da111e4462e2
Kerri Palphreyman <kap031@bucknell.edu>
2019-11-22 11:07:45 -0500

    formatting

--- csci205FinalProject/src/View/PropertyView.java
+++ csci205FinalProject/src/View/PropertyView.java
@@ -6,6 +6,10 @@ public class PropertyView {
 
     static Button buyPropertyButton;
 
+    /**
+     * adds a property button to the view
+     * @author - justin
+     */
     public static void addPropertyBuyButton() {
         buyPropertyButton = new Button("Buy \nProperty");
 
